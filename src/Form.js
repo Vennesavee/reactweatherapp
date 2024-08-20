a904e528ac091cab1baebe96620ff8e22a3cdbef
Vennesavee <126670814+Vennesavee@users.noreply.github.com>
2024-08-20 14:24:55 +0300

    updated Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-import "bootstrap/dist/css/bootstrap.css";
+
 
 export default function Form() {
   return (
